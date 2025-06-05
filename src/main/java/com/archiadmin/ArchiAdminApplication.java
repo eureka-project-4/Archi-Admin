f62927845752d5fc09a914cd530732ea4b168e95
1seyoung <akdlrnal0705@gmail.com>
2025-06-06 04:12:38 +0900

    init : first commit

---
+++ src/main/java/com/archiadmin/ArchiAdminApplication.java
@@ -0,0 +1,13 @@
+package com.archiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArchiAdminApplication.class, args);
+  }
+
+}
