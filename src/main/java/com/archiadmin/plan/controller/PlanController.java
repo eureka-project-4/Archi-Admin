2304eaa70c773615fe34637ddb7b5a4656cf9525
guntaek <rjsxor7584@naver.com>
2025-06-09 14:52:07 +0900

    chore : PlanController 에서 '/api' prefix 제거

--- src/main/java/com/archiadmin/plan/controller/PlanController.java
+++ src/main/java/com/archiadmin/plan/controller/PlanController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/plans")
+@RequestMapping("/plans")
 @RequiredArgsConstructor
 public class PlanController {
     private final PlanService planservice;
